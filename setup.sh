cd611d3ea0d51359bf6864c0238f8f8bfa8d05e0
Zhe Fu <fooozhe@gmail.com>
2017-09-02 14:39:46 -0700

    add setup scripts

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# link files
+echo "link files.."
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+# install vim plugins
+echo "install vim plugins.."
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/kshenoy/vim-signature.git
